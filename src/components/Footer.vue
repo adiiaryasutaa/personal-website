2d2d90fbe74791b68fc2a0ed803655c3a84439b4
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2022-06-17 00:08:21 +0800

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,6 @@
 <template>
 	<footer>
-		<div
-			class="container flex flex-col items-baseline text-slate-400 text-xs space-y-2 mx-auto p-4 sm:flex-row sm:justify-between"
-		>
+		<div class="container flex flex-col items-baseline text-slate-400 text-xs space-y-2 mx-auto p-4 sm:flex-row sm:justify-between">
 			<div class="font-capriola">©2022 Adi Aryasuta</div>
 			<div class="font-quicksand font-bold">
 				Built with
@@ -11,22 +9,14 @@
 					href="https://vuejs.org/"
 					target="_blank"
 					rel="noopener noreferrer"
-					>Vue.js</a
-				>,
+					>Vue.js </a
+				>and
 				<a
 					class="text-sky-400 hover:text-sky-500 active:text-sky-600 dark:text-sky-600 dark:hover:text-sky-700 dark:active:text-sky-800"
 					href="https://tailwindcss.com/"
 					target="_blank"
 					rel="noopener noreferrer"
 					>TailwindCSS</a
-				>
-				and
-				<a
-					class="text-blue-400 hover:text-blue-500 active:text-blue-600 dark:text-blue-600 dark:hover:text-blue-700 dark:active:text-blue-800"
-					href="https://flowbite.com/"
-					target="_blank"
-					rel="noopener noreferrer"
-					>Flowbite</a
 				>
 				with hosted on
 				<a
