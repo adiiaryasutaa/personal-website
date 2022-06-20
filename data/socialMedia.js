5b48e9b3427affe3feca981d2ce66e2828d19ec9
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2022-06-20 18:30:33 +0800

    merge data

--- data/socialMedia.js
+++
@@ -1,6 +0,0 @@
-export default {
-	github: "https://github.com/adiiaryasutaa",
-	linkedIn: "https://www.linkedin.com/in/adi-aryasuta-32870723b/",
-	instagram: "https://www.instagram.com/adiiaryasutaa/",
-	twitter: "https://twitter.com/lolcecep",
-};
