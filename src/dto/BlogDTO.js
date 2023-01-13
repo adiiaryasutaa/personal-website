36323cfeed59e274f60908b14b12181b3408547f
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2023-01-13 23:22:27 +0800

    Add blog card

---
+++ src/dto/BlogDTO.js
@@ -0,0 +1,9 @@
+export default class {
+	constructor(title, excerpt, category, image, date) {
+		this.title = title;
+		this.excerpt = excerpt;
+		this.category = category;
+		this.image = image;
+		this.date = date;
+	}
+}
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,13 +1,27 @@
 <script setup>
 import MainSection from '@/components/sections/MainSection.vue';
+import ContainerTitle from '@/components/ContainerTitle.vue';
+import Container from '@/components/Container.vue';
+import blogDTO from '@/dto/BlogDTO';
+import BlogCard from '@/components/cards/BlogCard.vue';
+
+const blog = new blogDTO('an example blog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa urna, feugiat non nisi vitae, tincidunt lobortis ipsum. Etiam varius metus eu nunc molestie maximus. Morbi vel dictum arcu. Curabitur tristique magna ut arcu...', 'technology', 'https://source.unsplash.com/user/c_v_r/1900x800', '2 days ago');
 </script>
 
 <template>
 	<main-section>
-		<section class="flex justify-center container mt-12">
-			<div class="text-center text-xl font-medium text-slate-600 rounded bg-slate-200 px-8 py-2 dark:text-slate-400 dark:bg-slate-800">
-				{{ $t('in-development') }}
+		<container>
+			<template #title>
+				<container-title>{{ $t('blog') }}</container-title>
+			</template>
+			<div class="grid grid-cols-1 gap-4 mt-4 sm:grid-cols-2 md:mt-6 lg:mt-8 xl:grid-cols-3 xl:gap-8 xl:mt-10">
+				<blog-card :blog="blog"/>
+				<blog-card :blog="blog"/>
+				<blog-card :blog="blog"/>
+				<blog-card :blog="blog"/>
+				<blog-card :blog="blog"/>
+				<blog-card :blog="blog"/>
 			</div>
-		</section>
+		</container>
 	</main-section>
 </template>
