b862f34e945afdc46e438133c69db4fad05dc7d5
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2023-01-14 21:26:45 +0800

    Add background image

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@ import Navbar from '@/components/Navbar.vue';
 </script>
 
 <template>
-	<div class="bg-gray-100 dark:bg-gray-900 min-h-screen">
+	<div class="bg-gray-100 bg-image-light min-h-screen dark:bg-gray-900 dark:bg-image-dark">
 		<header>
 			<navbar/>
 		</header>
 		<router-view/>
 	</div>
 </template>
+
+<style scoped>
+
+</style>
