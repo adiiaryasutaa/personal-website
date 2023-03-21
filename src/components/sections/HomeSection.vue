401714ff843c932a4ae6a60f06aa151cf86f7173
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2023-03-21 22:34:19 +0800

    Move home section to separate component

---
+++ src/components/sections/HomeSection.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <section class="container pt-16">
+    <div class="flex flex-col items-center">
+      <div class="">
+        <img src="@/assets/imgs/adiaryasuta.jpg" alt="dd"
+             class="rounded-full w-40 ring-8 ring-slate-200 dark:ring-slate-700">
+      </div>
+      <div class="flex flex-col items-center mt-8">
+        <h1 class="text-3xl text-slate-800 dark:text-slate-200">{{ $t('name') }}</h1>
+        <p v-html="$t('description')" class="font-medium text-sm text-slate-500 text-center leading-relaxed mt-4 md:text-base dark:text-slate-400"></p>
+        <router-link to="/about"
+                     class="px-4 py-2 text-sm font-medium border border-slate-300 rounded mt-8 hover:bg-slate-100 active:bg-slate-200 focus:ring-2 focus:ring-slate-200 dark:text-slate-300 dark:border-slate-600 dark:hover:bg-slate-800 dark:active:bg-slate-700 dark:focus:ring-slate-700">
+          {{ $t('see-more') }}
+        </router-link>
+      </div>
+    </div>
+  </section>
+</template>
