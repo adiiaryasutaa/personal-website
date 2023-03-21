8e29bf5f6717d471551bc5d3ebc5a893686b14de
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2023-03-21 15:35:48 +0800

    Update picture description

--- src/components/sections/AboutSection.vue
+++ src/components/sections/AboutSection.vue
@@ -8,9 +8,8 @@
 			<div class="flex flex-col items-center md:shrink-0">
 				<img src="@/assets/imgs/adiaryasuta2.jpg"
 						 class="rounded w-60 border-8 border-slate-200 xl:w-72 dark:border-slate-800">
-				<div class="text-xs mt-2 text-slate-400 dark:text-slate-600">
-					{{ $t('Adi Aryasuta at SMK Negeri 1 Denpasar') }}
-				</div>
+				<div v-html="$t('about-image-description-1')" class="text-xs mt-2 text-slate-400 dark:text-slate-600"></div>
+				<div v-html="$t('about-image-description-2')" class="text-xs mt-1 text-slate-400 dark:text-slate-600"></div>
 			</div>
 			<div class="mt-10 text-justify leading-relaxed text-slate-700 space-y-4 dark:text-slate-400 md:mt-0 md:ml-10 xl:text-lg">
 				<p>{{ $t('about') }}</p>
