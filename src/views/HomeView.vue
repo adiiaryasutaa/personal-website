ec4702d9642ff896465d7278ccd56fc3e33b5d6f
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2023-03-21 15:35:00 +0800

    Update description

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,9 +12,7 @@ import MainSection from '@/components/sections/MainSection.vue';
 				</div>
 				<div class="flex flex-col items-center mt-8">
 					<h1 class="text-3xl text-slate-800 dark:text-slate-200">{{ $t('name') }}</h1>
-					<p class="font-medium text-sm text-slate-500 text-center leading-relaxed mt-4 md:text-base dark:text-slate-400">
-						{{ $t('description') }}
-					</p>
+					<p v-html="$t('description')" class="font-medium text-sm text-slate-500 text-center leading-relaxed mt-4 md:text-base dark:text-slate-400"></p>
 					<router-link to="/about"
 											 class="px-4 py-2 text-sm font-medium border border-slate-300 rounded mt-8 hover:bg-slate-100 active:bg-slate-200 focus:ring-2 focus:ring-slate-200 dark:text-slate-300 dark:border-slate-600 dark:hover:bg-slate-800 dark:active:bg-slate-700 dark:focus:ring-slate-700">
 						{{ $t('see-more') }}
