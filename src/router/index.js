793efb325f8a95c9b15958e83cc5f3d94d4c0d78
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2023-03-22 14:28:03 +0800

    Redirect to home if route not found

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
 import ProjectView from '@/views/ProjectView.vue';
 import BlogView from '@/views/BlogView.vue';
+import * as path from "path";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
 			name: 'blog',
 			component: BlogView,
 		},
+		{
+			path: '/:pathMatch(.*)*',
+			name: 'not-found',
+			redirect: { name: 'home' }
+		}
 	],
 	scrollBehavior(to, from, savedPosition) {
 		document.getElementById('app').scrollIntoView({ behavior: 'smooth' })
