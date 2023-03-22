7202cdc96388ca09394b6c5dc5ee2bf62751e4ae
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2023-03-22 15:07:39 +0800

    Move social medias link to bottom of description

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup>
 import MainSection from '@/components/sections/MainSection.vue';
 import HomeSection from "@/components/sections/HomeSection.vue";
-import SocialMediaSection from "@/components/sections/SocialMediaSection.vue";
 </script>
 
 <template>
 	<main-section class="h-screen">
     <home-section />
-    <social-media-section class="mt-40" />
   </main-section>
 </template>
