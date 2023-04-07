b34a70f2eb4afd1b8eb23f5d7f355e71e4121a1f
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2023-04-07 19:56:11 +0800

    Update for transition and view height

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,17 @@ import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
-	<div class="bg-gray-100 bg-image-light min-h-screen selection:bg-slate-300 dark:bg-gray-900 dark:bg-image-dark dark:selection:bg-slate-700">
-  	<navbar/>
-		<router-view v-slot="{ Component }">
-			<transition name="fade" mode="in-out">
-				<component :is="Component"/>
-			</transition>
-		</router-view>
-		<Footer />
+	<div class="bg-gray-100 bg-image-light overflow-hidden selection:bg-slate-300 dark:bg-gray-900 dark:bg-image-dark dark:selection:bg-slate-700">
+  	<div class="flex flex-col min-h-screen">
+			<navbar/>
+			<div class="grow">
+				<router-view v-slot="{ Component, route }">
+					<transition name="fade" mode="out-in">
+						<component :is="Component" :key="route.path"/>
+					</transition>
+				</router-view>
+			</div>
+			<Footer/>
+    </div>
 	</div>
 </template>
-
-<style scoped>
-.fade-enter-active, .fade-leave-active {
-	transition: opacity 0.25s ease;
-}
-
-.fade-enter-from, .fade-leave-to {
-	opacity: 0;
-}
-</style>
